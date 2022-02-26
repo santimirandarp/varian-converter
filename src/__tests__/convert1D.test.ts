5b36745cdf8700b791670e45ae05145509d52db4
santi <santimir@pm.me>
2022-02-26 17:00:46 +0000

    added tests

--- src/__tests__/convert1D.test.ts
+++ src/__tests__/convert1D.test.ts
@@ -1,14 +1,25 @@
 import { join } from 'path';
+import {readFileSync} from 'fs';
 
-import { convert1D } from '../convert1D';
+import { convert1DFromDir } from '../convert1DFromDir';
+import { convert1DFromZip } from '../convert1DFromZip';
 
 describe('convert fid file for protons', () => {
   it('proton.fid', () => {
     const base = join(__dirname, '../../data/proton.fid');
-    const result = convert1D(base);
-    //    console.log(result.meta);
+    const result = convert1DFromDir(base);
+    expect(Object.keys(result.meta)).toHaveLength(9);
+    expect(Object.keys(result.fid)).toHaveLength(10);
+   // expect(result).toMatchSnapshot();
+  });
+  it('proton.fid.zip', async() => {
+    const ab= await readFileSync(join(__dirname, '../../data/proton.zip'));
+    const result = await convert1DFromZip(ab);
     expect(Object.keys(result.meta)).toHaveLength(9);
     expect(Object.keys(result.fid)).toHaveLength(10);
     expect(result).toMatchSnapshot();
   });
 });
+
+
+
